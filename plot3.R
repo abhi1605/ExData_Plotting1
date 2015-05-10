49ad40d11c5da4397739a655eb9264ad67b7cbdd
abhi1605 <abhinaya.sinha@gmail.com>
2015-05-10 01:46:35 -0700

    plot 3 added

---
+++ plot3.R
@@ -0,0 +1,6 @@
+png("plot3.png")
+plot(data$dateTime,data$Sub_metering_1,type="n",xlab = "",ylab = "Energy sub metering")
+lines(data$dateTime,data$Sub_metering_1)
+lines(data$dateTime,data$Sub_metering_2,col="red")
+lines(data$dateTime,data$Sub_metering_3,col="blue")
+legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty=c(1,1,1), col=c("black", "red", "blue"))
